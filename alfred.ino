ab494358e3a5ce730874aa7b27beabfc7580cfcc
Halehsan <halehuni@gmail.com>
2024-06-26 11:08:57 +0330

    refactor[core] : add a function for initializing the motors

--- alfred.ino
+++ alfred.ino
@@ -1,3 +1,4 @@
+
 // Motor A connections- right
 int enA = 5;
 int in1 = 7;
@@ -10,36 +11,41 @@ int in3 = 8;
 
 
 void setup() {
-// Set all the motor control pins to outputs
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
+
+  initializeMotors();
+  motor_function(0,0);
+}
+
+void initializeMotors() {
+
+  // Set all the motor control pins to output
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
 
-// Initialize the motors to stop
-  motor_function(0,0);
+  // stop
+  motor_function(0, 0);
 }
-
-
 
 
 void motor_function(int left_speed, int right_speed) {
 
-
+    
   analogWrite(enB, abs(left_speed));
   analogWrite(enA, abs(right_speed));
 
   // A - left side 
-
+  
   if (left_speed > 0) {
 
     digitalWrite(in3, HIGH);
     digitalWrite(in4, LOW);
 
   } else if (left_speed < 0) {
-
+    
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
   } else {
@@ -47,7 +53,7 @@ void motor_function(int left_speed, int right_speed) {
     digitalWrite(in3, LOW);
     digitalWrite(in4, LOW);
   }
-
+  
 
   // B - right side 
 
@@ -68,7 +74,7 @@ void motor_function(int left_speed, int right_speed) {
 
   }
 
-
+ 
 }
 
 
